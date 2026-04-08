5dd1ad0cf987c8fca0a2249607135e29330e8cca
holden.anderson <holden.anderson@students.snow.edu>
2026-04-07 18:44:18 -0600

    //A1 you have to run through the levels of interface

--- FInalBattler/Character/Upgrades/Item.cs
+++ FInalBattler/Character/Upgrades/Item.cs
@@ -1,4 +1,6 @@
-﻿namespace FinalBattler.Character.Upgrades
+﻿using System.ComponentModel;
+
+namespace FinalBattler.Character.Upgrades
 {
     public class Item
     {
@@ -14,6 +16,10 @@
         {
             ItemName = name;
             Description = description;
+            //A1 you have to run through the levels, interface says i must calculate damage
+            //2 what do you damage people with
+            //3 where does that thing get its info
+            //B1 how do people do damage to you? polymorphically
         }
     }
 }
